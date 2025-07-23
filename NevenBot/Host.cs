9d19b8d559a2e4b8d560aed6cf854152e1d6f907
Alexandr <86582717+tseneven@users.noreply.github.com>
2025-07-23 19:17:35 +0500

    Initial commit

---
+++ NevenBot/Host.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Host
+{
+    private static TelegramBotClient? _botClient;
+    private static ReceiverOptions? _receiverOptions;
+    static public Action<ITelegramBotClient, Update>? OnMessage;
+
+    
+    // Метод бота
+    static public Task Start(string token)
+    {
+        _botClient = new TelegramBotClient(token);
+        _receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = new[]
+            {
+                UpdateType.Message,
+            },
+        };
+
+        using var cts = new CancellationTokenSource();
+
+        _botClient.StartReceiving(UpdateHandler, ErrorHandler, _receiverOptions, cts.Token); // Запускаем бота
+
+        Task.Delay(-1); // Бесконечная задержка
+        return Task.CompletedTask;
+    }
+
+    // Метод обработки ошибок бота
+    static private Task ErrorHandler(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
+    {
+        var ErrorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Console.WriteLine(ErrorMessage);
+        return Task.CompletedTask;
+    }
+
+    // Метод обработки обновлений бота
+    static private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+    {
+        try
+        {
+            switch (update.Type)
+            {
+                case UpdateType.Message:
+                {
+                        Console.WriteLine($"Лог {update.Message?.Text ?? "Not Text"}");
+                        OnMessage?.Invoke(client, update);
+                        return;
+                }
+
+            }
+            await Task.CompletedTask;
+        }
+        catch (Exception ex)
+        { 
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
---
+++ NevenBot/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace NevenBot
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string token = GetToken();
+
+            if (token != "Error: Token not found")
+            {
+                // Инициализация бота
+                Host.Start(token);
+                Host.OnMessage += OnMessage;
+                Console.ReadKey();
+
+            }
+            else
+            {
+                Console.WriteLine(@"Ошибка, отсутствует токен или appsettings.json в папке \bin\Debug\net8.0\");
+            }
+
+        }
+
+        // Метод действия бота на сообщение пользователя. Сюда основной функционал и вызов методов API и работы с DB
+        private static async void OnMessage(ITelegramBotClient client, Update update)
+        {
+            await client.SendMessage(update.Message?.Chat.Id ?? 8152895676, "Текст");
+        }
+
+        // Метод получения токена из appsettings.json
+        static private string GetToken()
+        {
+            try
+            {
+                var builder = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+                IConfiguration config = builder.Build();
+
+                string token = config["ApiToken"];
+
+                return token;
+
+            }
+            catch
+            {
+                return "Error: Token not found";
+
+            }
+        }
+    }
+}
